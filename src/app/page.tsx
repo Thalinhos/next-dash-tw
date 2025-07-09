4aa4b7bfc46426579de10c9153a348af4fc6a036
Thalinhos <thalissonvieira@hotmail.com.br>
2025-07-09 12:36:06 -0300

    gateway in / route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+import { validarToken } from "@/utils/jwt";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <div className="container mx-auto py-8">
-    </div>
-  )
+export default async function Home() {
+  const cookieStore = await cookies()
+  const token = cookieStore.get('token')?.value;
+
+  if (!token) {
+    return redirect('/login');
+  }
+
+  try {
+      const isValid = await validarToken(token);
+  
+      if (!isValid) {
+        return redirect('/login')
+      }
+    } catch (e) {
+      return redirect('/login')
+
+    }
+  return redirect('/dashboard')
 }
